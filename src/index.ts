c27a53927d3b14d7548f1887afa2640a98322bbb
officegarden <84883494+officegarden@users.noreply.github.com>
2024-05-12 15:30:04 +0000

    feat: added custom name rules

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import sheriff from "eslint-config-sheriff";
-import { defineFlatConfig } from "eslint-define-config";
-import type { SheriffSettings } from "@sherifforg/types";
+import sheriff from 'eslint-config-sheriff';
+import { defineFlatConfig } from 'eslint-define-config';
+import type { SheriffSettings } from '@sherifforg/types';
 
 const sheriffOptions: SheriffSettings = {
   react: false,
@@ -15,12 +15,89 @@ export default defineFlatConfig([
   ...sheriff(sheriffOptions),
   {
     rules: {
-      'func-style': ['error', 'declaration'],
+      'func-style': [
+        'error', 
+        'declaration'
+      ],
       '@typescript-eslint/naming-convention': [
-        {
-          selector: 'import',
-          format: ['PascalCase']
-        }
+        'error', 
+        [
+          {
+            selector: 'default',
+            format: [
+              'camelCase'
+            ],
+            leadingUnderscore: 'forbid',
+            trailingUnderscore: 'forbid'
+          },
+          {
+            selector: 'import',
+            format: [
+              'CamelCase',
+            ],
+            leadingUnderscore: 'forbid',
+            trailingUnderscore: 'forbid'
+          },
+          {
+            selector: 'variable',
+            format: [
+              'camelCase',
+              'UPPER_CASE'
+            ],
+            modifiers: [
+                'const'
+            ],
+            types: [
+              'boolean',
+              'string',
+              'number'
+            ],
+            leadingUnderscore: 'forbid',
+            trailingUnderscore: 'forbid'
+          },
+          {
+            selector: 'objectLiteralProperty',
+            format: null,
+            leadingUnderscore: 'allowSingleOrDouble',
+            trailingUnderscore: 'forbid'
+          },
+          {
+            selector: 'typeLike',
+            format: [
+              'PascalCase'
+            ],
+            leadingUnderscore: 'forbid',
+            trailingUnderscore: 'forbid'
+          },
+          {
+            selector: 'variable',
+            types: [
+              'boolean'
+            ],
+            format: [
+              'camelCase'
+            ],
+            prefix: [
+              'is',
+              'has',
+              'should',
+              'can'
+            ],
+            leadingUnderscore: 'forbid',
+            trailingUnderscore: 'forbid'
+          },
+          {
+            selector: 'variable',
+            modifiers: [
+              'destructured'
+            ],
+            format: null
+          },
+          {
+            selector: 'typeProperty',
+            format: null
+          }
+        ]
       ]
     }
   }
